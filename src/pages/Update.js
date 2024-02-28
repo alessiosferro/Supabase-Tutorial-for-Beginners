a2caab20eee5068ab8fb5ad9882f74b07f02cf50
Alessio Sferro <sferro.alessio@gmail.com>
2024-02-28 12:12:10 +0100

    feat: add create smoothie page, update smoothie page

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,9 +1,74 @@
+import {useNavigate, useParams} from "react-router-dom";
+import {useCallback, useEffect, useState} from "react";
+import supabase from "../config/supabase-client";
+
 const Update = () => {
-  return (
-    <div className="page update">
-      <h2>Update</h2>
-    </div>
-  )
+    const {id} = useParams();
+    const navigate = useNavigate();
+
+    const [title, setTitle] = useState("");
+    const [method, setMethod] = useState("");
+    const [rating, setRating] = useState("");
+    const [formError, setFormError] = useState(null);
+
+    const fetchSmoothie = useCallback(async() => {
+        const {data, error} = await supabase
+            .from('smoothies')
+            .select()
+            .eq('id', id)
+            .single();
+
+        if (error) {
+            navigate('/', { replace: true });
+        }
+
+        if (data) {
+            setTitle(data.title);
+            setMethod(data.method);
+            setRating(data.rating);
+
+            console.log({ data });
+        }
+    }, [id, navigate])
+
+    useEffect(() => {
+        void fetchSmoothie();
+    }, [fetchSmoothie]);
+
+    const handleSubmit = () => {}
+
+    return (
+        <div className="page update">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="title">Title:</label>
+                <input
+                    type="text"
+                    id="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label htmlFor="method">Method:</label>
+                <textarea
+                    id="method"
+                    value={method}
+                    onChange={(e) => setMethod(e.target.value)}
+                />
+
+                <label htmlFor="rating">Rating:</label>
+                <input
+                    type="number"
+                    id="rating"
+                    value={rating}
+                    onChange={(e) => setRating(e.target.value)}
+                />
+
+                <button>Update Smoothie Recipe</button>
+
+                {formError && <p className="error">{formError}</p>}
+            </form>
+        </div>
+    )
 }
 
 export default Update
