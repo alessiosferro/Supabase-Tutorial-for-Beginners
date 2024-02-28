835ecb60ed69c457a9004193355bcc4bad918338
Alessio Sferro <sferro.alessio@gmail.com>
2024-02-28 11:19:45 +0100

    feat: add smoothies grid

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,58 @@
+import supabase from "../config/supabase-client";
+import {useEffect, useState} from "react";
+
+import SmoothieCard from "../components/SmoothieCard";
+
 const Home = () => {
-  return (
-    <div className="page home">
-      <h2>Home</h2>
-    </div>
-  )
+    const [fetchError, setFetchError] = useState(null);
+    const [smoothies, setSmoothies] = useState(null);
+
+    const fetchSmooties = async () => {
+        const {
+            data,
+            error
+        } = await supabase
+            .from('smoothies')
+            .select();
+
+        if (error) {
+            setFetchError('Could not fetch the smoothies');
+            setSmoothies(null);
+            console.log(error);
+        }
+
+        if (data) {
+            setSmoothies(data);
+            setFetchError(null);
+        }
+    }
+
+    useEffect(() => {
+        void fetchSmooties();
+    }, []);
+
+    return (
+        <div className="page home">
+            <h2>Home</h2>
+
+            {fetchError && (<p>{fetchError}</p>)}
+
+            {smoothies && (
+                <div className="smoothies">
+                    {/* order-by buttons */}
+                    <div className="smoothie-grid">
+                        {smoothies.map(smoothie => (
+                            <SmoothieCard
+                                key={smoothie.id}
+                                smoothie={smoothie}
+                            />
+                        ))}
+                    </div>
+                </div>
+            )}
+        </div>
+    )
 }
+
 
 export default Home
